d98963359d1eba925df199bfa153e5dbd460a2a3
Bharat Kumar <kumarbharat878@gmail.com>
2021-11-21 15:45:36 +0530

    current weather showing

--- app/src/main/java/com/bharat/weatherapp/MainActivity.kt
+++ app/src/main/java/com/bharat/weatherapp/MainActivity.kt
@@ -4,19 +4,22 @@ import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.bharat.weatherapp.R
+import com.bharat.weatherapp.databinding.ActivityMainBinding
 import com.bharat.weatherapp.repository.WeatherRepository
 import com.bharat.weatherapp.viewmodel.WeatherViewModel
 import com.bharat.weatherapp.viewmodel.WeatherViewModelFactory
 
 class MainActivity : AppCompatActivity() {
     private lateinit var weatherViewModel: WeatherViewModel
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         val repository = WeatherRepository()
         weatherViewModel = ViewModelProvider(this, WeatherViewModelFactory(repository,
@@ -29,6 +32,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun observeData() {
         weatherViewModel.getCurrentWeatherDetails().observe(this, Observer {
+//            binding.currentWeather = it
+            binding.txtCity.text = it.name
+
+            val temp = it.main?.temp
+            if (temp != null) {
+                binding.txtTemp.text = (temp - 273.15).toInt().toString()
+            }
             Log.d("current temp ", it.toString())
         })
 
